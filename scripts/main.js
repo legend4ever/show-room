9e3985ca5bd2c51e53b72901c4cda5be981e29ae
sy-demo <k1cka5543v3r@gmail.com>
2016-03-31 15:39:56 -0500

    update

--- scripts/main.js
+++ scripts/main.js
@@ -7,6 +7,17 @@ core_1.enableProdMode();
 browser_1.bootstrap(app_component_1.AppComponent, [router_1.ROUTER_PROVIDERS,
     core_1.provide(router_1.LocationStrategy, { useClass: router_1.HashLocationStrategy }),
     core_1.provide(Window, { useValue: window })]).then(function () {
+    var toggle = document.getElementsByClassName('navbar-toggle')[0];
+    var collapse = document.getElementsByClassName('navbar-collapse')[0];
+    var menuItem = document.getElementsByClassName('navbar-nav a');
+    var toggleMenu = function () {
+        collapse.classList.toggle('collapse');
+        collapse.classList.toggle('in');
+    };
+    toggle.addEventListener('click', toggleMenu, false);
+    for (var i = 0; i < menuItem.item.length; i++) {
+        menuItem.item(i).addEventListener('click', toggleMenu, false);
+    }
     window.onscroll = function () {
         var featureEl = document.getElementById('featured');
         var style = window.getComputedStyle(featureEl);
