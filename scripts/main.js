6a071dc9e4229fcf7cda826aed8e19568129033b
sy-demo <k1cka5543v3r@gmail.com>
2016-04-01 14:30:05 -0500

    update

--- scripts/main.js
+++ scripts/main.js
@@ -9,28 +9,40 @@ browser_1.bootstrap(app_component_1.AppComponent, [router_1.ROUTER_PROVIDERS,
     core_1.provide(Window, { useValue: window })]).then(function () {
     var toggle = document.getElementsByClassName('navbar-toggle')[0];
     var collapse = document.getElementsByClassName('navbar-collapse')[0];
-    var menuItem = document.querySelectorAll('.navbar-nav a');
+    var anchors = document.querySelectorAll('.navbar-nav a');
     var toggleMenu = function (e) {
         collapse.classList.toggle('collapse');
         collapse.classList.toggle('in');
         if (e.srcElement instanceof HTMLAnchorElement) {
-            console.log("Start processing...");
-            for (var i2 = 0; i2 < menuItem.length; i2++) {
-                var item = menuItem[i2];
-                item.parentElement.classList.remove('active');
-                console.log(item.hash + " <==> " + e.srcElement.hash);
-                if (item.hash === e.srcElement.hash)
-                    item.parentElement.classList.add('active');
+            for (var i2 = 0; i2 < anchors.length; i2++) {
+                var a = anchors[i2];
+                a.parentElement.classList.remove('active');
+                if (a.hash === e.srcElement.hash && e.srcElement.hash !== "#")
+                    a.parentElement.classList.add('active');
             }
         }
     };
     toggle.addEventListener('click', toggleMenu, false);
-    for (var i = 0; i < menuItem.length; i++) {
-        menuItem[i].addEventListener('click', toggleMenu, false);
+    for (var i = 0; i < anchors.length; i++) {
+        anchors[i].addEventListener('click', toggleMenu, false);
     }
     window.onscroll = function () {
         var featureEl = document.getElementById('featured');
         var style = window.getComputedStyle(featureEl);
+        var anchors = document.querySelectorAll('.navbar-nav a');
+        for (var i = 0; i < anchors.length; i++) {
+            var a = anchors[i];
+            var ref = a.getAttribute("href").substr(1);
+            if (ref !== '') {
+                var e = document.getElementById(ref);
+                if (e.offsetTop <= window.scrollY && e.offsetTop + e.clientHeight > window.scrollY) {
+                    a.parentElement.classList.add('active');
+                }
+                else {
+                    a.parentElement.classList.remove('active');
+                }
+            }
+        }
         var el = document.querySelector('header nav');
         //console.log(' ==> ' + window.scrollY + ' | ' + Number(style.getPropertyValue('height').replace('px', '')));
         if (window.scrollY >= Number(style.getPropertyValue('height').replace('px', ''))) {
