7ddfc8585c3ba1b62a20d3821c14b419d67fdfb5
sy-demo <k1cka5543v3r@gmail.com>
2016-04-02 14:30:06 -0500

    update

--- scripts/main.js
+++ scripts/main.js
@@ -54,5 +54,26 @@ browser_1.bootstrap(app_component_1.AppComponent, [router_1.ROUTER_PROVIDERS,
             el.classList.remove("mini-bar");
         }
     };
+    // safai shim
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] || window[vendors[x] + 'CancelRequestAnimationFrame'];
+    }
+    if (!window.requestAnimationFrame) {
+        window.requestAnimationFrame = (function (callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function () { callback(currTime + timeToCall); }, timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        });
+    }
+    ;
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function (id) {
+            clearTimeout(id);
+        };
 });
 //# sourceMappingURL=main.js.map
